53e2f5131e58e47ff09e486c44273e801896f24e
hjhbjh <jhbjhb>
2026-03-23 06:28:47 +0000

    Code challenge submission for question: 70 Climbing Stairs

---
+++ 70 Climbing Stairs/70climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int , memo={}) -> int:
+        if memo is None:
+            memo={}
+        if n==0 or n==1:
+            return 1
+        elif n>=2:
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n]= self.climbStairs(n-1,memo) + self.climbStairs(n-2,memo)
+                return memo[n]
+        
