115aa633453cebb9d9e5dc1ffeba3a5440fc59b7
Harsh <harshyadav2308@gmail.com>
2026-07-25 15:56:44 +0530

    Add solution for Find Peak Element

--- LeetCode/Find-Peak-Element/Solution.py
+++ LeetCode/Find-Peak-Element/Solution.py
@@ -1,17 +1,12 @@
 1class Solution:
 2    def findPeakElement(self, nums: List[int]) -> int:
-3        n = len(nums)
-4
-5        if n == 1:
-6            return 0
-7
-8        if nums[0] > nums[1]:
-9            return 0
-10        
-11        if nums[-1] > nums[-2]:
-12            return n-1
-13        
-14        for i in range(1,n-1):
-15            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-16                return i
-17
+3        i = 0
+4        j = len(nums) - 1 
+5        while i < j:
+6            mid = (i+j)//2 
+7            if nums[mid] > nums[mid+1]:
+8                j = mid 
+9            else:
+10                i = mid + 1 
+11        return i 
+12            
