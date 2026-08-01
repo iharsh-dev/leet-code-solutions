732c043115f5ac03528500147a9a5d71b51f05cc
Harsh <harshyadav2308@gmail.com>
2026-08-01 10:18:40 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.py
+++ LeetCode/Climbing-Stairs/Solution.py
@@ -1,14 +1,12 @@
-1from collections import defaultdict
+1from functools import cache
 2class Solution:
 3    def climbStairs(self, n: int) -> int:
-4        memo = defaultdict(int)
+4        @cache
 5        def dp(step):
 6            if step == n:
 7                return 1
 8            if step > n:
 9                return 0
-10            if not memo[step]:
-11                memo[step] = dp(step+1)+dp(step+2) 
-12            return memo[step]
-13        return dp(0)
-14
+10            return dp(step+1)+dp(step+2) 
+11        return dp(0)
+12
