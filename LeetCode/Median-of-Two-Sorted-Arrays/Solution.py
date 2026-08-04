2f47f3194b54c4f610049318aa29777c732a1815
Harsh <harshyadav2308@gmail.com>
2026-08-04 21:45:31 +0530

    Add solution for Median of Two Sorted Arrays

--- LeetCode/Median-of-Two-Sorted-Arrays/Solution.py
+++ LeetCode/Median-of-Two-Sorted-Arrays/Solution.py
@@ -1,25 +1,9 @@
-1double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-2    int i=0,j=0,k=0;
-3    int num=nums1Size+nums2Size;
-4    double arr[num];
-5    while(i<nums1Size && j<nums2Size){
-6       if(*(nums1+i)<*(nums2+j)){
-7        arr[k++]=*(nums1+i++);
-8       }
-9       else{
-10        arr[k++]=*(nums2+j++);
-11       }
-12    }
-13    while(j<nums2Size){
-14        arr[k++]=*(nums2+j++);
-15    }
-16    while(i<nums1Size){
-17        arr[k++]=*(nums1+i++);
-18    }
-19    if(num%2!=0){
-20        return arr[(num)/2];
-21    }
-22    else{
-23        return (arr[(num)/2]+arr[(num)/2-1])/2.0;
-24    }
-25}
+1class Solution:
+2    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+3        new = nums1+nums2
+4        new.sort()
+5        n = len(new)
+6        if n % 2 == 0:
+7            return (new[(n-1)//2]+new[n//2])/2 
+8        
+9        return new[n//2]
