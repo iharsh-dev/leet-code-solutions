11558168e13cbdbc206ab207e149cd432fa215f9
Harsh <harshyadav2308@gmail.com>
2026-08-07 00:54:52 +0530

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.py
+++ LeetCode/Find-the-Duplicate-Number/Solution.py
@@ -1,10 +1,15 @@
-1from collections import defaultdict
-2class Solution:
-3    def findDuplicate(self, nums: List[int]) -> int:
-4        freq = defaultdict(int)
-5
-6        for i in nums:
-7            if not freq[i]:
-8                freq[i] = 1 
-9            else:
-10                return i
+1class Solution:
+2    def findDuplicate(self, nums: List[int]) -> int:
+3        fast = nums[0]
+4        slow = nums[0]
+5        while True:
+6            slow = nums[slow]
+7            fast = nums[nums[fast]]
+8            if slow == fast:
+9                break
+10
+11        slow = nums[0]
+12        while slow!=fast:
+13            fast = nums[fast]
+14            slow = nums[slow]
+15        return slow
