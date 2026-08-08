e22408566dc6f1aa5ad0d911b929a06ff29cb18c
Harsh <harshyadav2308@gmail.com>
2026-08-08 13:52:02 +0530

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.py
+++ LeetCode/Valid-Palindrome/Solution.py
@@ -1,14 +1,9 @@
 1class Solution:
 2    def isPalindrome(self, s: str) -> bool:
-3        a = []
-4        for i in s:
-5            o = ord(i)
-6            if 65 <= o <= 90:
-7                a.append(chr(o + 32))
-8            elif 97 <= o <= 122 or 48 <= o <= 57:
-9                a.append(i)
-10        if a == a[::-1]:
-11            return True
-12        else:
-13            return False
-14
+3        a = s.lower()
+4        b = "".join([char for char in a if char.isalnum()])
+5        if b == b[::-1]:
+6            return True 
+7        
+8        return False
+9
