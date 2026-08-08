b0d33574dee24720209940186013538827e0e3b0
Harsh <harshyadav2308@gmail.com>
2026-08-08 18:41:13 +0530

    Add solution for Search Insert Position

--- LeetCode/Search-Insert-Position/Solution.py
+++ LeetCode/Search-Insert-Position/Solution.py
@@ -1,1 +1,13 @@
-Input: nums = [1,3,5,6], target = 5
+1class Solution:
+2    def searchInsert(self, nums: List[int], target: int) -> int:
+3        left = 0
+4        right = len(nums) - 1
+5        while left <= right:
+6            mid = (left + right)//2
+7            
+8            if nums[mid] >= target:
+9                right = mid - 1
+10            else:
+11                left = mid + 1
+12        return left
+13
