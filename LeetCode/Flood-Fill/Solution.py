27718672950c089ec60d02a39fba22f3fdb89fda
Harsh <harshyadav2308@gmail.com>
2026-08-09 18:32:25 +0530

    Add solution for Flood Fill

--- LeetCode/Flood-Fill/Solution.py
+++ LeetCode/Flood-Fill/Solution.py
@@ -1,27 +1,24 @@
-class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int m = image.size();
-        int n = image[0].size();
-        queue<pair<int, int>> q;
-        q.push(make_pair(sr, sc));
-        int startColor = image[sr][sc];
-        if (startColor == newColor) return image;
-        int e[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        image[sr][sc] = newColor;
-        while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for (int k = 0; k < 4; k++) {
-                int x = r + e[k][0];
-                int y = c + e[k][1];
-                if (x >= 0 && y >= 0 && x < m && y < n && image[x][y] == startColor) {
-                    image[x][y] = newColor;
-                    q.push(make_pair(x, y));
-                }
-            }
-        }
-        return image;
-    }
-};
+1from collections import deque
+2class Solution:
+3    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+4        n = len(image)
+5        m = len(image[0])
+6
+7        direction = [(0,1),(1,0),(0,-1),(-1,0)]
+8
+9        org = image[sr][sc]
+10        if org == color:
+11            return image
+12        
+13        image[sr][sc] = color
+14        queue = deque()
+15        queue.append((sr,sc))
+16
+17        while queue:
+18            i, j = queue.popleft()
+19
+20            for x, y in direction:
+21                if 0 <= i + x < n and 0 <= j + y < m and image[i + x][j + y] == org:
+22                    queue.append(( i + x,j + y))
+23                    image[x + i][j + y] = color
+24        return image
