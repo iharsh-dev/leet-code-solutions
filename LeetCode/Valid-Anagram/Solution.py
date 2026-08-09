e33192ce3e90b87519271f69f61cdfb36de72d1b
Harsh <harshyadav2308@gmail.com>
2026-08-09 16:58:41 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.py
+++ LeetCode/Valid-Anagram/Solution.py
@@ -1,7 +1,7 @@
 1from collections import Counter
 2class Solution:
 3    def isAnagram(self, s: str, t: str) -> bool:
-4        if Counter(s) == Counter(t):
+4        if sorted(s) == sorted(t):
 5            return True
 6        
 7        return False
