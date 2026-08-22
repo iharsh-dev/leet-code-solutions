95d375d5c5635e8e9f22c2090c17218a164156bc
Harsh <harshyadav2308@gmail.com>
2026-08-22 23:47:45 +0530

    Add solution for Check Divisibility by Digit Sum and Product

--- LeetCode/Check-Divisibility-by-Digit-Sum-and-Product/Solution.py
+++ LeetCode/Check-Divisibility-by-Digit-Sum-and-Product/Solution.py
@@ -1,11 +1,10 @@
 1class Solution:
 2    def checkDivisibility(self, n: int) -> bool:
-3        arr = [int(i) for i in str(n)]
-4        prod = 1
-5        summ = 0
-6        for i in arr:
-7            prod *= i
-8            summ += i 
-9        
-10        return True if not n % (prod + summ) else False
-11
+3        prod = 1
+4        summ = 0
+5        for i in str(n):
+6            prod *= int(i)
+7            summ += int(i) 
+8        
+9        return True if not n % (prod + summ) else False
+10
